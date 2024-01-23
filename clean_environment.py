49b901bbc6fd11baaefa7b662a044d10ad46eeca
Gilad Ouzan <142928003+giladouzan1@users.noreply.github.com>
2024-01-23 17:32:55 +0200

    clean_env fix

--- clean_environment.py
+++ clean_environment.py
@@ -1,16 +1,14 @@
 import requests
-#
-# try:
-#     response = requests.get('http://127.0.0.1:5000/stop_server')
-#     response.raise_for_status()  # Raise an exception for non-200 status codes
-# except requests.exceptions.RequestException as e:
-#     print(f"Error stopping server at 127.0.0.1:5000: {e}")
-#
-# try:
-#     response = requests.get('http://127.0.0.1:5001/stop_server')
-#     response.raise_for_status()
-# except requests.exceptions.RequestException as e:
-#     print(f"Error stopping server at 127.0.0.1:5001: {e}")
 
-res = requests.delete(f'http://127.0.0.1:5000/users/8')
-assert res.status_code == 200
+try:
+    response = requests.get('http://127.0.0.1:5000/stop_server')
+    response.raise_for_status()  # Raise an exception for non-200 status codes
+except requests.exceptions.RequestException as e:
+    print(f"Error stopping server at 127.0.0.1:5000: {e}")
+
+try:
+    response = requests.get('http://127.0.0.1:5001/stop_server')
+    response.raise_for_status()
+except requests.exceptions.RequestException as e:
+    print(f"Error stopping server at 127.0.0.1:5001: {e}")
+
